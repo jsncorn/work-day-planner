36d15ac273ca3ab660849f78a658fdd8d5cd35d0
jsncorn <jsncourn@gmail.com>
2022-01-07 18:34:19 -0800

    cleaned up js file

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-var timeBox = $('#currentTime');
-let constantTime;
-
 //constantly get time every second and upload it
 function getTime() {
+    var timeBox = $('#currentTime');
     var timeInterval = setInterval(function(){
         timeBox.text(moment().format("MMMM Mo YYYY, h:mm:ss A"))
     }, 1000)
