551385b0aae8eb07e0ae842b3fd0ed1de2d0bde5
jsncorn <jsncourn@gmail.com>
2022-01-07 18:32:47 -0800

    added all text blocks w/ comments; will dynamically add time to blocks

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var timeBox = $('#currentTime');
 let constantTime;
 
+//constantly get time every second and upload it
 function getTime() {
     var timeInterval = setInterval(function(){
         timeBox.text(moment().format("MMMM Mo YYYY, h:mm:ss A"))
     }, 1000)
 }
 getTime();
+
